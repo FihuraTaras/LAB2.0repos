257e34277b9a4b1214e0bb0fa4f06100b98cc187
FihuraTaras <figura.taras@gmail.com>
2021-09-30 13:07:04 +0300

    first commit

---
+++ Lab2.0.cpp
@@ -0,0 +1,27 @@
+﻿// Lab_02.cpp
+// Автор: Ткачишин Юрій.
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 17.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double m; // вхідний параметр
+	double n; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "m = "; cin >> m;
+	cout << "n = "; cin >> n;
+	z1 = (sqrt(((3.0 * m + 2.0)*(3.0 * m + 2.0) - (24.0 * m)))/((3.0 * sqrt(m))-((2.0)/(sqrt(m)))));	
+	z2 = sqrt(m);
+	//Robin hood is hero.(3rd commit on branch master)
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
